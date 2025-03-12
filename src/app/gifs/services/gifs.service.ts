484221c10a263fd4fa5729bebe6b3f41678f3c1c
Carlos Tencio <danits252016@gmail.com>
2025-03-12 12:05:00 -0600

    InfiniteScroll finish

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -22,8 +22,20 @@ export class GifService {
 
   private http = inject(HttpClient);
 
-  trendingGifs = signal<Gif[]>([]);
-  trendingGifsLoading = signal(true);
+  trendingGifs = signal<Gif[]>([]);//[gif,gif,gif]
+  trendingGifsLoading = signal(false);
+  private trendingPage = signal(0);
+  //[[fig,fig,fig],[fig,fig,fig],[fig,fig,fig]]
+
+  trendingGifGroup = computed<Gif[][]>(() => {//con esto creo grupos de 3 para mostrar en el grid
+    const groups = [];
+    for (let i = 0; i < this.trendingGifs().length; i += 3) {
+      groups.push(this.trendingGifs().slice(i, i + 3));
+    }
+
+    return groups;
+  })
+
 
   searchHistory = signal<Record<string, Gif[]>>(loadFromLocalStorage());
   searchHistoryKeys = computed(() => Object.keys(this.searchHistory()));
@@ -41,14 +53,20 @@ export class GifService {
 
 
   loadTrendingGifs() {
+    if (this.trendingGifsLoading()) return;
+
+    this.trendingGifsLoading.set(true);
+
     this.http.get<GiphyResponse>(`${environment.giphyUrl}/gifs/trending`, {
       params: {
         api_key: environment.giphyapiKey,
         limit: '20',
+        offset: this.trendingPage() * 20
       }
     }).subscribe((resp) => {
       const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);//con esto se puede ver el arreglo de una mejor manera
-      this.trendingGifs.set(gifs);
+      this.trendingGifs.update((currentGifs)=>[...currentGifs,...gifs]);
+      this.trendingPage.update((page) => page + 1);
       this.trendingGifsLoading.set(false);
       console.log(gifs);
     });
